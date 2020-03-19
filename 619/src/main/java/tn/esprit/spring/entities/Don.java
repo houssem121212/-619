11a245f3df01ed78e2b5f053a7415ee8b53a6e4e
aymenjaballah <aymen.jaballah@esprit.tn>
2020-03-19 02:18:12 +0100

    premiere commit entité

--- 619/src/main/java/tn/esprit/spring/entities/Don.java
+++ 619/src/main/java/tn/esprit/spring/entities/Don.java
@@ -1,8 +1,16 @@
 package tn.esprit.spring.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 @Entity
-public class Don {
-
+public class Don implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_don;
+	private long montant_don;
+	private String description ;
 }
