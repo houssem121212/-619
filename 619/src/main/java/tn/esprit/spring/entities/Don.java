ece983ad7c404fdff944b5b7833d6cdb5b9f8dd8
aymenjaballah <aymen.jaballah@esprit.tn>
2020-03-19 02:27:31 +0100

    aymen commit entite event

--- 619/src/main/java/tn/esprit/spring/entities/Don.java
+++ 619/src/main/java/tn/esprit/spring/entities/Don.java
@@ -13,4 +13,64 @@ public class Don implements Serializable {
 	private long id_don;
 	private long montant_don;
 	private String description ;
+	public long getId_don() {
+		return id_don;
+	}
+	public void setId_don(long id_don) {
+		this.id_don = id_don;
+	}
+	public long getMontant_don() {
+		return montant_don;
+	}
+	public void setMontant_don(long montant_don) {
+		this.montant_don = montant_don;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (id_don ^ (id_don >>> 32));
+		result = prime * result + (int) (montant_don ^ (montant_don >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Don other = (Don) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id_don != other.id_don)
+			return false;
+		if (montant_don != other.montant_don)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Don [id_don=" + id_don + ", montant_don=" + montant_don + ", description=" + description + "]";
+	}
+	public Don(long id_don, long montant_don, String description) {
+		super();
+		this.id_don = id_don;
+		this.montant_don = montant_don;
+		this.description = description;
+	}
+	public Don() {
+		super();
+	}
 }
