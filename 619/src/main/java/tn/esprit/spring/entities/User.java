111273642c739ccaa7eea1a1414bcf197ff3de24
houssemalimi <houssem.alimi@esprit.tn>
2020-03-22 14:47:27 +0200

    user update

--- 619/src/main/java/tn/esprit/spring/entities/User.java
+++ 619/src/main/java/tn/esprit/spring/entities/User.java
@@ -1,213 +1,161 @@
 package tn.esprit.spring.entities;
 
-import java.util.List;
 
-import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.NaturalId;
+
 
 @Entity
-@Table(name = "User")
-public class User {
-	
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+            "username"
+        }),
+        @UniqueConstraint(columnNames = {
+            "email"
+        })
+})
+public class User  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String userName;
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
     private String password;
-    private boolean active;
-    private String roles;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+    
     
     @OneToMany(mappedBy="user")
-	private List<Command> Commands;
+  	private List<Command> Commands;
+      
+      @OneToMany(mappedBy="user")
+  	private List<Delivery> deliverys;
+      
+      @OneToMany(mappedBy="user")
+  	private List<Don> dons;
+      
+      @OneToOne(mappedBy="user")
+  	private Aisle aisle;
+      
+      @ManyToMany(mappedBy="users")
+  	private List<Event> events;
+
+      @ManyToOne
+  	private Stock stocks;
+      
+      @OneToOne
+  	private Cart cart;
+      
+      @OneToOne(mappedBy="user")
+  	private Ads ads;
+      
+      @ManyToMany(mappedBy="users")
+  	private List<Product> products;
+      
     
-    @OneToMany(mappedBy="user")
-	private List<Delivery> deliverys;
-    
-    @OneToMany(mappedBy="user")
-	private List<Don> dons;
-    
-    @OneToOne(mappedBy="user")
-	private Aisle aisle;
-    
-    @ManyToMany(mappedBy="users")
-	private List<Event> events;
 
-    @ManyToOne
-	private Stock stocks;
-    
-    @OneToOne
-	private Cart cart;
-    
-    @OneToOne(mappedBy="user")
-	private Ads ads;
-    
-    @ManyToMany(mappedBy="users")
-	private List<Product> products;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	public String getRoles() {
-		return roles;
-	}
-
-	public void setRoles(String roles) {
-		this.roles = roles;
-	}
-
-	public List<Command> getCommands() {
-		return Commands;
-	}
-
-	public void setCommands(List<Command> commands) {
-		Commands = commands;
-	}
-
-	public List<Delivery> getDeliverys() {
-		return deliverys;
-	}
-
-	public void setDeliverys(List<Delivery> deliverys) {
-		this.deliverys = deliverys;
-	}
-
-	public List<Don> getDons() {
-		return dons;
-	}
-
-	public void setDons(List<Don> dons) {
-		this.dons = dons;
-	}
-
-	public Aisle getAisle() {
-		return aisle;
-	}
-
-	public void setAisle(Aisle aisle) {
-		this.aisle = aisle;
-	}
-
-	public List<Event> getEvents() {
-		return events;
-	}
-
-	public void setEvents(List<Event> events) {
-		this.events = events;
-	}
-
-	public Stock getStocks() {
-		return stocks;
-	}
-
-	public void setStocks(Stock stocks) {
-		this.stocks = stocks;
-	}
-
-	public Cart getCart() {
-		return cart;
-	}
-
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
-
-	public Ads getAds() {
-		return ads;
-	}
-
-	public void setAds(Ads ads) {
-		this.ads = ads;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<Product> products) {
-		this.products = products;
-	}
-
-	public User(int id, String userName, String password, boolean active, String roles, List<Command> commands,
-			List<Delivery> deliverys, List<Don> dons, Aisle aisle, List<Event> events, Stock stocks, Cart cart, Ads ads,
-			List<Product> products) {
-		super();
-		this.id = id;
-		this.userName = userName;
-		this.password = password;
-		this.active = active;
-		this.roles = roles;
-		Commands = commands;
-		this.deliverys = deliverys;
-		this.dons = dons;
-		this.aisle = aisle;
-		this.events = events;
-		this.stocks = stocks;
-		this.cart = cart;
-		this.ads = ads;
-		this.products = products;
-	}
-
-	public User(String userName, String password, boolean active, String roles, List<Command> commands,
-			List<Delivery> deliverys, List<Don> dons, Aisle aisle, List<Event> events, Stock stocks, Cart cart, Ads ads,
-			List<Product> products) {
-		super();
-		this.userName = userName;
-		this.password = password;
-		this.active = active;
-		this.roles = roles;
-		Commands = commands;
-		this.deliverys = deliverys;
-		this.dons = dons;
-		this.aisle = aisle;
-		this.events = events;
-		this.stocks = stocks;
-		this.cart = cart;
-		this.ads = ads;
-		this.products = products;
-	}
-
-	public User() {
-		super();
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", userName=" + userName + ", active=" + active + ", roles=" + roles + ", Commands="
-				+ Commands + ", deliverys=" + deliverys + ", dons=" + dons + ", aisle=" + aisle + ", events=" + events
-				+ ", stocks=" + stocks + ", cart=" + cart + ", ads=" + ads + ", products=" + products + "]";
-	}
-
-    
-	
-    
    
+     
+    
+      
+    
 }
